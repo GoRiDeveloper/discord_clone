7b9eff12fbc3bde9172855a3920105df02999f40
GoRiJ <jolem824@gmail.com>
2023-11-15 19:43:26 -0600

    ci: remote patterns added for external images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['uploadthing.com', 'utfs.io'],
+        domains: ['utfs.io'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                pathname: '**',
+            },
+        ],
     },
 };
 
