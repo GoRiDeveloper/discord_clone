6b53cf9c9d4507db6d9d5c9e6b8711ab7115f147
GoRiJ <jolem824@gmail.com>
2023-11-19 21:15:47 -0600

    feat: path to remove a user from the server - delete route

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -5,8 +5,84 @@ import { currentProfile, db } from '@/lib';
 /**
  * Patch member parameters.
  */
-interface ParamsPatch {
+interface ParamsMemberId {
     params: { memberId: string };
+}
+
+/**
+ * Function to delete a user from the server in the database.
+ *
+ * @param { NextRequest } req - Next Request.
+ * @param { ParamsPatch } param1 - Patch parameters.
+ *
+ * @returns { Promise<NextResponse> } Response to delete a user from the server in the database.
+ */
+export async function DELETE(
+    req: NextRequest,
+    { params }: ParamsMemberId
+): Promise<NextResponse> {
+    try {
+        /**
+         * The current profile in session.
+         */
+        const profile = await currentProfile();
+
+        // Search parameters in the url.
+        const { searchParams } = new URL(req.url);
+
+        /**
+         * Server id from the search parameters in the url.
+         */
+        const serverId = searchParams.get('serverId');
+
+        // If there is no profile in session, return a non-authorization response.
+        if (!profile) return new NextResponse('Unauthorized', { status: 401 });
+
+        // If there is no server id, it returns a response that there is no server id.
+        if (!serverId)
+            return new NextResponse('Server ID missing', { status: 400 });
+
+        // If there is no member id, it returns a response that there is no member id.
+        if (!params.memberId)
+            return new NextResponse('Member ID missing', { status: 400 });
+
+        /**
+         *  Update members on server in database.
+         */
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id,
+            },
+            data: {
+                members: {
+                    deleteMany: {
+                        id: params.memberId,
+                        profileId: {
+                            not: profile.id,
+                        },
+                    },
+                },
+            },
+            include: {
+                members: {
+                    include: {
+                        profile: true,
+                    },
+                    orderBy: {
+                        role: 'asc',
+                    },
+                },
+            },
+        });
+
+        // Return the server with the member's updated.
+        return NextResponse.json(server);
+    } catch (error) {
+        console.error('[MEMBER_ID_DELETE]', error);
+        // Return an error response if there is an error.
+        return new NextResponse('Internal Error', { status: 500 });
+    }
 }
 
 /**
@@ -19,7 +95,7 @@ interface ParamsPatch {
  */
 export async function PATCH(
     req: NextRequest,
-    { params }: ParamsPatch
+    { params }: ParamsMemberId
 ): Promise<NextResponse> {
     try {
         /**
