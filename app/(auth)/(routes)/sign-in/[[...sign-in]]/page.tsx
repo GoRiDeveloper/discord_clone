1d832e9c090bdbd6858710ae18f58cbc487647ee
GoRiJ <jolem824@gmail.com>
2024-01-18 13:19:34 -0600

    feat: redirect auth route

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,13 @@
 import type { FC, JSX } from 'react';
 import { SignIn } from '@clerk/nextjs';
 
+import { BASE_URL } from '@/models';
+
 /**
  * User Registration Modal Component.
  *
  * @returns { JSX.Element } User Registration Modal Component.
  */
-const SignInPage: FC = (): JSX.Element => <SignIn />;
+const SignInPage: FC = (): JSX.Element => <SignIn redirectUrl={BASE_URL} />;
 
 export default SignInPage;
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,13 @@
-import type { FC, JSX } from 'react';
 import { SignUp } from '@clerk/nextjs';
+import type { FC, JSX } from 'react';
+
+import { BASE_URL } from '@/models';
 
 /**
  * User Authentication Modal Component.
  *
  * @returns { JSX.Element } User Authentication Modal Component.
  */
-const SignUpPage: FC = (): JSX.Element => <SignUp />;
+const SignUpPage: FC = (): JSX.Element => <SignUp redirectUrl={BASE_URL} />;
 
 export default SignUpPage;
