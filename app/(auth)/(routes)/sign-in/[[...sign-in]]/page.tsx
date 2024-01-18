6f340eade11908fd634483e53096a66ddd40430d
GoRiJ <jolem824@gmail.com>
2024-01-18 13:34:42 -0600

    feat: clerk url

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,15 @@
-import type { FC, JSX } from 'react';
 import { SignIn } from '@clerk/nextjs';
+import type { FC, JSX } from 'react';
 
 import { BASE_URL } from '@/models';
-
+console.log(BASE_URL);
 /**
  * User Registration Modal Component.
  *
  * @returns { JSX.Element } User Registration Modal Component.
  */
-const SignInPage: FC = (): JSX.Element => <SignIn afterSignInUrl={BASE_URL} />;
+const SignInPage: FC = (): JSX.Element => (
+    <SignIn redirectUrl={BASE_URL} afterSignInUrl={BASE_URL} />
+);
 
 export default SignInPage;
