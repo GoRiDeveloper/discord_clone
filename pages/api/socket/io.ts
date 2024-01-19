0db9d98a12ec0febaf570f6ce2c990cad6f8d464
GoRiJ <jolem824@gmail.com>
2024-01-19 00:08:52 -0600

    feat: websocket adition

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -38,6 +38,7 @@ const ioHandler = (
                 credentials: true,
             },
             connectTimeout: 10000,
+            transports: ['websocket'],
         });
 
         res.socket.server.io = io;
